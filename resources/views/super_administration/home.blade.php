459d4d3c85ddf241adbf0a2fbd133c7018e07acd
benji <benja.bendo02@gmail.com>
2020-08-16 00:37:55 +0100

    initalisation des bases par rapport au super_administrateur

---
+++ resources/views/super_administration/home.blade.php
@@ -0,0 +1,1 @@
+<h1>Bienvenue dans la super administration</h1>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::view('/','template_super_administrateur');
+
+Route::group(['prefix'=>'surperadmin'],function (){
+
+    Route::view('/','super_administration.home');
+
 });
