7cac8988cce2a03ab971481b3faefe5516089709
benji <benja.bendo02@gmail.com>
2020-10-25 23:06:35 +0100

    typo oauth

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
@@ -25,19 +26,7 @@ class LoginController extends Controller
     public function githubRedirect()
     {
         $user = Socialite::driver('github')->user();
-        /*
-         * Si utilisateur existe pas ajouter le
-         * Si utilisateur exite recuper ces informations puis connecter lui
-         * dans les deux cas, authentifiez l'utilisateur dans l'application et redirigez-le ensuite
-         * */
-        $user = User::firstOrCreate([
-            'email'=>$user->getEmail()
-        ],[
-            'name'=>$user->getNickname(),
-            'profile_photo_path'=>$user->getAvatar(),
-            'password'=> Hash::make(Str::random(24)),
-        ]);
-        $user->assignRole('etudiant');
+        $this->roleUser($user);
         Auth::login($user,true);
         return redirect('/profil');
     }
@@ -56,17 +45,33 @@ class LoginController extends Controller
     }
     public function googleRedirect(){
         $user = Socialite::driver('google')->user();
-//        dd($user->getAvatar());
-        $user = User::firstOrCreate([
-            'email'=>$user->getEmail()
-        ],[
-            'name'=>$user->name,
-            'profile_photo_path'=>$user->getAvatar(),
-            'password'=> Hash::make(Str::random(24)),
-        ]);
-        $user->assignRole('etudiant');
-        Auth::login($user,true);
+        $this->roleUser($user);
+        Auth::login( $user,true);
         return redirect('/profil');
     }
 
+    private function saveUser($user){
+        /*
+         * Si utilisateur existe pas ajouter le
+         * Si utilisateur exite recuper ces informations puis connecter lui
+         * dans les deux cas, authentifiez l'utilisateur dans l'application et redirigez-le ensuite
+         * */
+        return User::firstOrCreate([
+            'email'=>$user->getEmail()
+        ],[
+            'name'=>$user->getNickname(),
+            'profile_photo_path'=>$user->getAvatar(),
+            'password'=> Hash::make(Str::random(24)),
+        ]);
+    }
+    private function roleUser($user){
+        $nbUser = DB::table('users')->count();
+        if ($nbUser === 0) {
+            $user = $this->saveUser($user);
+            $user->assignRole('super-admin');
+        } else {
+            $user = $this->saveUser($user);
+            $user->assignRole('etudiant');
+        }
+    }
 }
