8e2416fcc8697e86620e90af68469d865ea47823
benji <benja.bendo02@gmail.com>
2020-10-25 18:02:09 +0100

    google oauth

--- config/services.php
+++ config/services.php
@@ -40,13 +40,14 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'http://your-callback-url',
+        'redirect' => env('FACEBOOK_REDIRECT_URL'),
     ],
 
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://your-callback-url',
+        'redirect' => env('GOOGLE_REDIRECT_URL'),
+//        'redirect' => 'http://your-callback-url',
     ],
 
 
