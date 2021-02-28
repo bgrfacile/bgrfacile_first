3aa0a863ee04679eb3d4038091f1dd72b1a087b1
benji <benaja.bendo02@gmail.com>
2021-02-28 21:12:15 +0100

    change domaine

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,10 +37,8 @@ class LoginController extends Controller
     }
     public function facebookRedirect(){
         $user = Socialite::driver('facebook')->user();
-//        dd($user);
-//        $user= $this->roleUser($user);
-//        $faker = \Faker\Factory::create('fr')->unique()->safeEmail;
-        $user->email =  $faker;
+        dd($user);
+//        $user->email =  $faker;
         Auth::login( $user,true);
         return redirect('/profil');
     }
